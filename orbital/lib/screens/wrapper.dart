f7da5979e4eefd25ef78f43eed0f3d07425ab9c7
Yong <e0425848@u.nus.edu>
2020-05-18 16:12:51 +0800

    added home, authentication, wrapper file

---
+++ orbital/lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:orbital/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Return home/authenticate widget;
+    return Home();
+  }
+}
