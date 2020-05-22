9097693d69c318860c5f15843e1aa4c713d0a887
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-05-23 01:21:05 +0800

    Add tab bar for navigation in activity_feed

--- orbital/lib/screens/activity_feed/activity_feed.dart
+++ orbital/lib/screens/activity_feed/activity_feed.dart
@@ -4,15 +4,34 @@ import 'package:flutter/cupertino.dart';
 
 import '../app_drawer.dart';
 
-class ActivityFeed extends StatelessWidget  {
-
+class ActivityFeed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(title: Text('Activity Feed'), centerTitle: true),
-      body: new Text('Display List of CCAs queried from Cloud Firebase'),
-      drawer: AppDrawer(),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: new AppBar(
+            title: Text('Activity Feed'),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.whatshot),
+                  child: Text('All'),
+                ),
+                Tab(
+                  icon: Icon(Icons.star),
+                  child: Text('My Favourites'),
+                )
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Text('All: Display List of all CCAs queried from Cloud Firebase'),
+              Text('Favourite: Display List of favourite CCAs queried from Cloud Firebase')
+            ],),
+          drawer: AppDrawer(),
+        ));
   }
-
 }
