ca49660099e610f182b73fd53ebcceef8b4bb70e
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-11 00:21:00 +0800

    Create cca about page for normal users (not admin).

---
+++ orbital/lib/cca/cca_normal_about.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:orbital/services/auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CCANormalAbout extends StatelessWidget {
+  final DocumentSnapshot document;
+
+  CCANormalAbout({this.document});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final String category = document['Category'];
+    final String description = document['Description'];
+    final String email = document['Email'];
+
+    return ListView(children: [
+      Container(height: 50, child:Card(
+          margin: EdgeInsets.all(5),
+          elevation: 1.0,
+          shadowColor: Colors.blue,
+          child: Text(category, style: TextStyle(fontSize: 20)))), //category
+      Card(
+          margin: EdgeInsets.all(5),
+          elevation: 1.0,
+          shadowColor: Colors.blue,
+          child: Text(description, style: TextStyle(fontSize: 20))), //description
+      Container(height: 50, child: Card(
+          margin: EdgeInsets.all(5),
+          elevation: 1.0,
+          shadowColor: Colors.blue,
+          child: Text(email, style: TextStyle(fontSize: 20)))), //email
+      Card(), //external sites
+    ]);
+  }
+}
