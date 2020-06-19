a24a8ed89e32892071eee92e784cbeba187d5d28
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-19 23:25:18 +0800

    Fix textform fields disappearing when text too long bug by changing ListView to SingleChildScrollView.

--- orbital/lib/cca/create_event.dart
+++ orbital/lib/cca/create_event.dart
@@ -14,7 +14,7 @@ class CreateEvent extends StatefulWidget {
 class _CreateEventState extends State<CreateEvent> {
   String _name, _time, _details, _location;
   final GlobalKey<FormState> _key = GlobalKey();
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,87 +24,86 @@ class _CreateEventState extends State<CreateEvent> {
         ),
         body: Center(
             child: Form(
-          autovalidate: true,
-          key: _key,
-          child: ListView(children: <Widget>[
-            SizedBox(height: 10),
-            Container(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                    'Complete and submit the form below to create a new Event',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ))),
-            SizedBox(height: 20),
-            Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return 'Provide an Event name';
-                    }
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    hintText: 'My Event',
-                  ),
-                  onSaved: (input) => _name = input,
-                )),
-            SizedBox(height: 30),
-            Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  maxLines: null,
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return 'Provide event details';
-                    }
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Details',
-                    hintText: 'What the event is about',
-                  ),
-                  onSaved: (input) => _details = input,
-                )),
-            SizedBox(height: 30),
-            Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  maxLines: null,
-                  validator: (input) {
-                    if (input.isEmpty) {
-                      return 'Provide date/time of event';
-                    }
-                  },
-                  decoration: InputDecoration(
-                    labelText: 'Time',
-                    hintText: 'Date/Time',
-                  ),
-                  onSaved: (input) => _time = input,
-                )),
-            SizedBox(height: 30),
-            Container(
-                padding: EdgeInsets.all(8),
-                child: TextFormField(
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    labelText: 'Location',
-                    hintText: 'Leave empty if not applicable.',
-                  ),
-                  onSaved: (input) => _location = input,
-                )),
-            SizedBox(height: 30),
-            Container(
-              padding: EdgeInsets.all(8),
-              child: CupertinoButton.filled(
-                child: Text('Publish'),
-                onPressed: null,
-              ),
-            )
-          ]),
-        )));
+                autovalidate: true,
+                key: _key,
+                child: SingleChildScrollView(
+                  child: Column(children: <Widget>[
+                    SizedBox(height: 10),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        child: Text(
+                            'Complete and submit the form below to create a new Event',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                    SizedBox(height: 20),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        child: TextFormField(
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Provide an Event name';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                            hintText: 'My Event',
+                          ),
+                          onSaved: (input) => _name = input,
+                        )),
+                    SizedBox(height: 30),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        child: TextFormField(
+                          maxLines: null,
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Provide event details';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Details',
+                            hintText: 'What the event is about',
+                          ),
+                          onSaved: (input) => _details = input,
+                        )),
+                    SizedBox(height: 30),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        child: TextFormField(
+                          maxLines: null,
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Provide date/time of event';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: 'Time',
+                            hintText: 'Date/Time',
+                          ),
+                          onSaved: (input) => _time = input,
+                        )),
+                    SizedBox(height: 30),
+                    Container(
+                        padding: EdgeInsets.all(8),
+                        child: TextFormField(
+                          maxLines: null,
+                          decoration: InputDecoration(
+                            labelText: 'Location',
+                            hintText: 'Leave empty if not applicable.',
+                          ),
+                          onSaved: (input) => _location = input,
+                        )),
+                    SizedBox(height: 30),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      child: CupertinoButton.filled(
+                        child: Text('Publish'),
+                        onPressed: null,
+                      ),
+                    )
+                  ]),
+                ))));
   }
-
-
 }
