363c878c2411a21a7596f3b756d81364e8765c9e
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-20 23:00:13 +0800

    Create event_admin_view.dart used for admins to view and edit events.

---
+++ orbital/lib/cca/event_admin_view.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:orbital/services/auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class EventAdminView extends StatelessWidget {
+  final DocumentSnapshot document;
+
+  EventAdminView({@required this.document});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(document['Name'], style: TextStyle(color: Colors.black)),
+          centerTitle: true,
+        ),
+        body: ListView(children: [
+          Container(
+              height: 50,
+              child: Card(
+                  margin: EdgeInsets.all(5),
+                  elevation: 1.0,
+                  shadowColor: Colors.blue,
+                  child: Text(document['Details'],
+                      style: TextStyle(fontSize: 20)))),
+          Card(
+              margin: EdgeInsets.all(5),
+              elevation: 1.0,
+              shadowColor: Colors.blue,
+              child:
+                  Text(document['DateTime'], style: TextStyle(fontSize: 20))),
+          Container(
+              height: 50,
+              child: Card(
+                  margin: EdgeInsets.all(5),
+                  elevation: 1.0,
+                  shadowColor: Colors.blue,
+                  child: Text(document['Location'],
+                      style: TextStyle(fontSize: 20)))),
+          SizedBox(height: 50),
+          CupertinoButton.filled(onPressed: null, child: Text('Sign Up')),
+        ]));
+  }
+}
