48bf3956ddc097de0a53f4d708f09a57b8503e6b
Yong <e0425848@u.nus.edu>
2020-06-21 15:40:14 +0800

    Improve UI for profile page

--- orbital/lib/screens/profile.dart
+++ orbital/lib/screens/profile.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:orbital/services/auth.dart';
 import 'app_drawer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 class Profile extends StatefulWidget {
@@ -70,13 +71,58 @@ class _ProfileState extends State<Profile> {
     super.initState();
   }
 
+    Widget actionButtons() {
+    return Padding(
+      padding: EdgeInsets.only(left: 25.0, right: 25.0, top: 30.0),
+      child: new Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 10.0),
+              child: Container(
+                  child: new RaisedButton(
+                child: new Text("Logout"),
+                textColor: Colors.white,
+                color: Colors.green,
+                onPressed: () {
+                  signOut();
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
+            ),
+            flex: 2,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(left: 5.0),
+              child: Container(
+                  child: new RaisedButton(
+                child: new Text("Change password"),
+                textColor: Colors.white,
+                color: Colors.red,
+                onPressed: () {
+                  showAlertDialog(context);
+                },
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(20.0)),
+              )),
+            ),
+            flex: 2,
+          ),
+        ],
+      ),
+    );
+  }
    @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: Text(
-          'Profile!',
-          style: TextStyle(color: Colors.blue),
+          'Profile',
+          style: TextStyle(color: Colors.black),
         ),
         centerTitle: true,
       ),
@@ -86,24 +132,15 @@ class _ProfileState extends State<Profile> {
         children: <Widget>[
           Image.asset(
                 "images/logo.png",
-                height: 200,
-                width: 200,
+                height: 250,
+                width: 250,
               ),
           SizedBox(height: 40),
-          Text('Name: ' + widget.auth.name, style: TextStyle(fontSize: 20,  height: 1.2 )),
+          Text('Name: ' + widget.auth.name, style: GoogleFonts.sriracha( textStyle: TextStyle(color: Colors.blue, letterSpacing: .5,fontSize: 25))),
           SizedBox(height: 20),
-          Text('Email: ' + widget.auth.email, style: TextStyle(fontSize: 15)),
+          Text('Email: ' + widget.auth.email, style: GoogleFonts.sriracha( textStyle: TextStyle(color: Colors.blue, letterSpacing: .5,fontSize: 30))),
           SizedBox(height: 20),
-          new RaisedButton(
-          onPressed: () => signOut(),
-          child: new Text(
-            "Logout",
-          )),
-          new RaisedButton(
-          onPressed: () => showAlertDialog(context),
-          child: new Text(
-            "Change password",
-          ))
+          actionButtons()
         ],
           
     )));
