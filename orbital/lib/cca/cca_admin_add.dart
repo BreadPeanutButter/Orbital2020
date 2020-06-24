10991da0d6b3bb5da7743a63bfebd21c468e51c9
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-24 17:29:55 +0800

    Create cca_admin_add.dart form used to add admins.

---
+++ orbital/lib/cca/cca_admin_add.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:orbital/services/auth.dart';
+
+class CCAAdminAdd extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _CCAAdminAddState();
+  }
+}
+
+class _CCAAdminAddState extends State<CCAAdminAdd> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
