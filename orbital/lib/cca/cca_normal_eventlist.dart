8e47f63867a2c13b16265b9f679c0e582f521c29
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-28 21:24:34 +0800

    Fix 'No event available message not showing up' bug.

--- orbital/lib/cca/cca_normal_eventlist.dart
+++ orbital/lib/cca/cca_normal_eventlist.dart
@@ -32,45 +32,46 @@ class CCANormalEventlist extends StatelessWidget {
           .orderBy('DateCreated', descending: true)
           .snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return new Text(
-              'No events available ☹️',
-              style: TextStyle(fontSize: 20),
-            );
-          default:
-            return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                return new SizedBox(
-                    height: 100,
-                    child: Card(
-                        shape: RoundedRectangleBorder(
-                            side:
-                                new BorderSide(color: Colors.grey, width: 1.0),
-                            borderRadius: BorderRadius.circular(4.0)),
-                        margin: EdgeInsets.all(3),
-                        elevation: 3.0,
-                        shadowColor: Colors.blue,
-                        child: InkWell(
-                            highlightColor: Colors.blueAccent,
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          EventNormalView(document: document)));
-                            },
-                            child: ListTile(
-                                title: new Text(
-                                    document['CCA'] + ': ' + document['Name'],
-                                    style: TextStyle(fontSize: 24)),
-                                subtitle: new Text(document['EventTime'],
-                                    style: TextStyle(fontSize: 20)),
-                                trailing: closedEvent(document)))));
-              }).toList(),
-            );
+        if (snapshot.hasError) {
+          return new Text('Error: ${snapshot.error}');
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.data.documents.isEmpty) {
+          return new Center(
+              child: Text(
+            'No events available ☹️',
+            style: TextStyle(fontSize: 30),
+          ));
+        } else {
+          return new ListView(
+            children: snapshot.data.documents.map((DocumentSnapshot document) {
+              return new SizedBox(
+                  height: 100,
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          side: new BorderSide(color: Colors.grey, width: 1.0),
+                          borderRadius: BorderRadius.circular(4.0)),
+                      margin: EdgeInsets.all(3),
+                      elevation: 3.0,
+                      shadowColor: Colors.blue,
+                      child: InkWell(
+                          highlightColor: Colors.blueAccent,
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        EventNormalView(document: document)));
+                          },
+                          child: ListTile(
+                              title: new Text(
+                                  document['CCA'] + ': ' + document['Name'],
+                                  style: TextStyle(fontSize: 24)),
+                              subtitle: new Text(document['EventTime'],
+                                  style: TextStyle(fontSize: 20)),
+                              trailing: closedEvent(document)))));
+            }).toList(),
+          );
         }
       },
     );
