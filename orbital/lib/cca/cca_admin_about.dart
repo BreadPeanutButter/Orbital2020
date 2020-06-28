e85779d49e088b671201ae1175e69171b7ae952c
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-06-28 15:56:20 +0800

    Remove redundant imports.

--- orbital/lib/cca/cca_admin_about.dart
+++ orbital/lib/cca/cca_admin_about.dart
@@ -1,13 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:orbital/cca/cca_admin_edit.dart';
-import 'package:orbital/services/auth.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter/src/painting/_network_image_io.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CCAAdminAbout extends StatelessWidget {
   final DocumentSnapshot document;
@@ -22,85 +17,106 @@ class CCAAdminAbout extends StatelessWidget {
     final String email = document['Contact'];
     final String imageURL = document['image'];
 
-    Widget getWidget(){
-      if(imageURL == null){
+    Widget getWidget() {
+      if (imageURL == null) {
         return SizedBox(height: 20);
-      }
-      else{
+      } else {
         return Image.network(
           imageURL,
           height: 200,
           width: 200,
-          );
+        );
       }
     }
 
-  BoxDecoration myBoxDecoration() {
-    return BoxDecoration(
-    border: Border.all(
-      color: Colors.red,
-      width: 3.0,
-    ),
-    borderRadius: BorderRadius.all(
-       Radius.circular(7.0)
-    ),
-
-  );
-}
-
-  Widget myWidget(String info) {
-    return Container(
-      margin: const EdgeInsets.all(1.0),
-      padding: const EdgeInsets.all(10.0),
-      decoration: myBoxDecoration(), 
-      child: Text(
-        info,
-        style: GoogleFonts.ptSans()
-      ),
-    );
-}
-
-
-
-  Widget helper() {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: ListView(
-          children: <Widget>[
-            getWidget(),
-            SizedBox(height: 5,),
-            Text("Category", style:  TextStyle(fontSize: 20, fontStyle:  FontStyle.italic, fontWeight: FontWeight.bold)),
-            SizedBox(height: 7,),
-            myWidget(category),
-            SizedBox(height: 6.0),
-            Text("Description", style:  TextStyle(fontSize: 20, fontStyle:  FontStyle.italic, fontWeight: FontWeight.bold)),
-            SizedBox(height: 7,),
-            myWidget(description),
-            SizedBox(height: 6.0),
-            Text("Email and contact:", style:  TextStyle(fontSize: 20, fontStyle:  FontStyle.italic, fontWeight: FontWeight.bold)),
-            SizedBox(height: 7,),
-            myWidget(email),
-            SizedBox(height: 50,),
-            RaisedButton.icon(
-                onPressed: (){ Navigator.push(context,MaterialPageRoute(builder: (c) => CCAAdminEdit(ccaDocument : document))); },
-                shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                label: Text('Edit', 
-                style: TextStyle(color: Colors.white, fontSize: 20),),
-                icon: Icon(Icons.edit, color: Colors.white,), 
-                textColor: Colors.red,
-                splashColor: Colors.red,
-                color: Colors.green,),
-            
-          ],
+    BoxDecoration myBoxDecoration() {
+      return BoxDecoration(
+        border: Border.all(
+          color: Colors.red,
+          width: 3.0,
         ),
-      )
-    );
-  }
+        borderRadius: BorderRadius.all(Radius.circular(7.0)),
+      );
+    }
 
-  
+    Widget myWidget(String info) {
+      return Container(
+        margin: const EdgeInsets.all(1.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: myBoxDecoration(),
+        child: Text(info, style: GoogleFonts.ptSans()),
+      );
+    }
+
+    Widget helper() {
+      return Card(
+          margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ListView(
+              children: <Widget>[
+                getWidget(),
+                SizedBox(
+                  height: 5,
+                ),
+                Text("Category",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 7,
+                ),
+                myWidget(category),
+                SizedBox(height: 6.0),
+                Text("Description",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 7,
+                ),
+                myWidget(description),
+                SizedBox(height: 6.0),
+                Text("Email and contact:",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 7,
+                ),
+                myWidget(email),
+                SizedBox(
+                  height: 50,
+                ),
+                RaisedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (c) =>
+                                CCAAdminEdit(ccaDocument: document)));
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  label: Text(
+                    'Edit',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  icon: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                  textColor: Colors.red,
+                  splashColor: Colors.red,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ));
+    }
 
     return Scaffold(
       body: helper(),
