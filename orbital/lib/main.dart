ccb2b3148773d2f8c07d4f42a52639b41adab325
BreadPeanutButter <nus@NUSs-MacBook-Pro.local>
2020-07-13 19:38:45 +0800

    Remove redundant imports.

--- orbital/lib/main.dart
+++ orbital/lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:orbital/cca/create_cca.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import "package:orbital/screens/profile.dart";
 import 'screens/event_feed/event_feed.dart';
 import 'screens/explore/explore.dart';
-import 'package:orbital/screens/authetication/sign_up.dart';
-import 'package:orbital/cca/create_event.dart';
-import 'package:orbital/cca/create_cca.dart';
-import 'package:orbital/cca/cca_normal_view.dart';
 import 'package:orbital/screens/authetication/sign_in.dart';
 
 void main() {
