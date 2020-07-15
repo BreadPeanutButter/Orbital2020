b260e1bd4909f5ff8142c8ed93637b6f4dce2f18
BreadPeanutButter <joonjie98@gmail.com>
2020-07-15 20:14:34 +0800

    Add maximum ccas dialog for cca creation button.

--- orbital/lib/cca/create_cca.dart
+++ orbital/lib/cca/create_cca.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:orbital/cca/cca_categories.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:orbital/services/auth.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
